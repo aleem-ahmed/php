f1334511e1f955d6d51f7c43599cbb5775a44464
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-01-16 16:19:45 -0500

    LMAOO

--- rptide/htdocs/common/top/top-desktop-menu.php
+++ rptide/htdocs/common/top/top-desktop-menu.php
@@ -9,15 +9,28 @@
 		if (isset($_SESSION['username'])):
 	?>
 
-		<li><a id="user-sign-out-button">↦ Sign Out</a></li>
-		<li><a id="show-user-account-button" style="border-style: solid; border-width: 1px; Border-color: #9bdc94;">➤ Your Account</a></li>
+		<li>
+			<a id="user-sign-out-button">↦ Sign Out</a>
+		</li>
+		<li>
+			<a
+				id="show-user-account-button"
+				style="border-style: solid; border-width: 1px; Border-color: #9bdc94;"
+			>➤ Your Account</a>
+		</li>
 
 	<?php
 		else:
 	?>    
-
-		<li><a id="show-sign-up-page-button">✎ Sign Up</a></li>
-		<li><a id="show-sign-in-page-button" style="border-style: solid; border-width: 1px; Border-color: #9bdc94;">➤ Sign In</a></li>
+		<li>
+			<a id="show-sign-up-page-button">✎ Sign Up</a>
+		</li>
+		<li>
+			<a
+				id="show-sign-in-page-button"
+				style="border-style: solid; border-width: 1px; Border-color: #9bdc94;"
+			>➤ Sign In</a>
+		</li>
 
 	<?php
 		endif;
--- rptide/htdocs/forex/index.php
+++ rptide/htdocs/forex/index.php
@@ -14,7 +14,9 @@
 		<!-- TradingView Widget BEGIN -->
 		<div class="tradingview-widget-container" style="margin: 0 auto;">
 		<div class="tradingview-widget-container__widget"></div>
-		<div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/currencies/forex-cross-rates/" rel="noopener" target="_blank"><span class="blue-text">Forex Rates</span></a> by TradingView</div>
+		<div class="tradingview-widget-copyright">
+		<a href="https://www.tradingview.com/markets/currencies/forex-cross-rates/" rel="noopener" target="_blank">
+		<span class="blue-text">Forex Rates</span></a> by TradingView</div>
 		<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-forex-cross-rates.js" async>
 		{
 		"width": 770,
